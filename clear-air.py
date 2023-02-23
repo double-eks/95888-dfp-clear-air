8044bdf4488ede0aa31d14384b76aa0ed595fd70
sean-xx <xiaoxu1094@gmail.com>
2023-02-23 01:06:57 -0500

    v0.1

--- clear-air.py
+++ clear-air.py
@@ -3,7 +3,27 @@ import logging
 import numpy as np
 import pandas as pd
 
-logging.basicConfig(level=logging.DEBUG)
+
+def genLogger(name: str, rank: int,
+              level: int = logging.DEBUG, formatting: str = ''):
+    if (name == ''):
+        raise Exception('invalid logger name')
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    tab = rank * '\t'
+    formatting = f'{tab}%(levelname)s >> %(message)s'
+    formatter = logging.Formatter(formatting, datefmt='%H:%M')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
 
 if __name__ == "__main__":
-    print('this is the prototype file')
+
+    # Initialize logging
+    logging.basicConfig(filename='processing.log',
+                        filemode='w',
+                        level=logging.DEBUG)
+    logger = genLogger('0', 0)

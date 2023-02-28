dff0e02c5d068fd5524d99759996ff2d6729a855
sean-x <xiaoxu@andrew.cmu.edu>
2023-02-28 06:17:40 -0500

    v3.6.3

--- trend.py
+++ trend.py
@@ -1,4 +1,5 @@
 import os
+from operator import index
 
 import numpy as np
 
@@ -22,142 +23,139 @@ Asthma Indictor Question Dictionary
 '''
 
 
-def genFieldQuery():
-    responses = ['yearstart', 'locationabbr', 'question',
-                 'datavalueunit', 'datavaluetype', 'datavalue',
-                 'stratificationcategory1', 'stratification1']
-    fields = ', '.join(responses)
-    return fields
-
-
-def genClauseQuery(state: str):
-    location = f'(locationabbr = "{state.upper()}")'
-    value = '(datavalue IS NOT NULL)'
-    # typeClause = genTextWhereClause(
-    #     'datavaluetype', 'OR', 'Age-adjusted Rate', 'Age-adjusted Prevalence')
-    typeClause = '(datavaluetype = "Number")'
-    questionClause = genTextWhereClause(
-        'question', 'OR', 'Asthma mortality rate',
-        'Current asthma prevalence among adults aged >= 18 years')
-    clause = ' AND '.join([location, value, typeClause, questionClause])
-    return clause
-
-
-def genTextWhereClause(field: str, operator: str, *args):
-    conditions = []
-    for arg in args:
-        currCondition = '{} = "{}"'.format(field, arg)
-        conditions.append(currCondition)
-    mergedClause = f' {operator} '.join(conditions)
-    mergedClause = f'({mergedClause})'
-    return mergedClause
-
-
-def convertResponse(response):
-    df = pd.DataFrame.from_records(response)
-    df = df.rename(columns={'yearstart': 'Year',
-                            'locationabbr': 'State',
-                            'datavaluetype': 'Type',
-                            'datavalue': 'Value',
-                            'datavalueunit': 'Unit',
-                            'stratificationcategory1': 'Demography',
-                            'stratification1': 'Group'})
-    df = df.astype({'Value': float, 'Year': int})
-    df = df.set_index('Year')
-    df = df.sort_index()
-    return df
-
-
 class cdcAPI:
 
     def __init__(self, state: str):
+        self.state = state
+        self.dash = '--'
+        self.initQuestionDict()
+        self.initClauseQuery()
         client = Socrata(domain='chronicdata.cdc.gov',
                          app_token='S1j1YaUvwuS3NZh6FDtLeDDFs',
                          username='xiaoxu@andrew.cmu.edu',
                          password='mqanS3iXwPfR2!E')
         datasetID = 'us8e-ubyj'
-        clause = genClauseQuery(state)
-        fields = genFieldQuery()
-        response = client.get(datasetID, where=clause, select=fields)
-        self.df = convertResponse(response)
-        self.start, self.end = 2010, 2020
-        self.state = state
-        self.dash = '--'
-        print(len(self.df))
+        fields = ['yearstart', 'locationabbr', 'question',
+                  'datavalueunit', 'datavaluetype', 'datavalue',
+                  'stratificationcategory1', 'stratification1']
+        fields = ', '.join(fields)
+        self.response = client.get(datasetID, where=self.clause, select=fields)
+        self.convertResponse()
 
-    def overallTrend(self):
-        questionDict = {'Asthma mortality rate': 'Mortality Rate',
-                        'Current asthma prevalence among '
-                        'adults aged >= 18 years': 'Prevalence among Adults'}
-        overallDf = self.df[self.df.Demography == 'Overall']
+    def initQuestionDict(self):
+        questionDict = {
+            'Asthma mortality rate':
+                ['Age-adjusted Rate', 'Asthma Mortality Rate'],
+            'Current asthma prevalence among adults aged >= 18 years':
+                ['Age-adjusted Prevalence', 'Prevalence of Adults with Current Asthma'],
+            'Emergency department visit rate for asthma':
+                ['Age-adjusted Rate', 'Asthma ED Visit Rate'],
+            'Hospitalizations for asthma':
+                ['Age-adjusted Rate', 'Asthma Hospitalizations Rate']}
+        questionDf = pd.DataFrame(questionDict,
+                                  index=['datavaluetype', 'rename'])
+        self.qDict = questionDf
 
-        fig, ax = plt.subplots()
-        xTicks = overallDf.index.unique()
-        ax.set_title(
-            f'Asthma Trend in {self.state} '
-            f'({xTicks.values.min()}-{xTicks.values.max()})',
-            fontweight='bold')
-        ax.set_xlabel('Year', fontweight='bold')
-        ax.set_ylabel('Z-score Standardization', fontweight='bold')
-        ax.set_xticks(xTicks)
-        ax.set_ylim(-2, 2)
-        ax.grid(True, linestyle=self.dash, alpha=0.5)
-        self.zScoreAx(ax, xTicks)
+    def initClauseQuery(self):
+        location = f'(locationabbr = "{self.state.upper()}") '
+        data = '(datavalue IS NOT NULL)'
+        quesstions = ['(question = "{}")'.format(factor)
+                      for factor in list(self.qDict.columns)]
+        questionClause = ' OR '.join(quesstions)
+        questionClause = '({})'.format(questionClause)
+        self.clause = ' AND '.join([location, data, questionClause])
 
-        for name, group in overallDf.groupby('question'):
-            data = group.Value
-            zscore = (data - data.mean(numeric_only=True)) / \
-                data.std(numeric_only=True)
-            ax.plot(zscore.index, zscore.values,
-                    label=questionDict[name], marker='.')
-        ax.legend(fontsize=10, loc='upper left')
-        plt.show()
+    def convertResponse(self):
+        df = pd.DataFrame.from_records(self.response)
+        typeRowFilter = []
+        types = []
+        for row in df.index:
+            currQuestion = df.loc[row, 'question']
+            currDataType = df.loc[row, 'datavaluetype']
+            if (currDataType == self.qDict[currQuestion]['datavaluetype']):
+                typeRowFilter.append(True)
+                types.append(self.qDict[currQuestion]['rename'])
+            else:
+                typeRowFilter.append(False)
+        df = df.loc[typeRowFilter, :]
+        df = df.astype({'datavalue': float, 'yearstart': int})
+        df = df.rename(columns={'yearstart': 'Year',
+                                'locationabbr': 'State',
+                                'datavaluetype': 'Type',
+                                'datavalue': 'Rate',
+                                'datavalueunit': 'Unit',
+                                'stratificationcategory1': 'Demography',
+                                'stratification1': 'Subgroup'})
+        df['Factor'] = types
+        df = df.set_index('Year')
+        self.df = df.sort_index()
+
+    # def overallTrend(self):
+    #     questionDict = {'Asthma mortality rate': 'Mortality Rate',
+    #                     'Current asthma prevalence among '
+    #                     'adults aged >= 18 years': 'Prevalence among Adults'}
+    #     overallDf = self.df[self.df.Demography == 'Overall']
+
+    #     fig, ax = plt.subplots()
+    #     xTicks = overallDf.index.unique()
+    #     ax.set_title(
+    #         f'Asthma Trend in {self.state} '
+    #         f'({xTicks.values.min()}-{xTicks.values.max()})',
+    #         fontweight='bold')
+    #     ax.set_xlabel('Year', fontweight='bold')
+    #     ax.set_ylabel('Z-score Standardization', fontweight='bold')
+    #     ax.set_xticks(xTicks)
+    #     ax.set_ylim(-2, 2)
+    #     ax.grid(True, linestyle=self.dash, alpha=0.5)
+    #     self.zScoreAx(ax, xTicks)
+
+    #     for name, group in overallDf.groupby('question'):
+    #         data = group.Value
+    #         zscore = (data - data.mean(numeric_only=True)) / \
+    #             data.std(numeric_only=True)
+    #         ax.plot(zscore.index, zscore.values,
+    #                 label=questionDict[name], marker='.')
+    #     ax.legend(fontsize=10, loc='upper left')
+    #     plt.show()
 
     def demography(self):
-        questionDict = {
-            'Asthma mortality rate': 'Asthma Mortality Rate',
-            'Current asthma prevalence among adults aged >= '
-            '18 years': 'Prevalence of Adults with Current Asthma (%)'}
-        groupsDf = self.df[self.df.Demography != 'Overall']
-        questions = groupsDf.question.unique()
-        groups = groupsDf.Demography.unique()
-        xTicks = groupsDf.index.unique()
-        yTicks = np.linspace(0, 35, 11)
+        df = self.df[self.df.Demography != 'Overall']
+        factors = df.Factor.unique()
+        demos = df.Demography.unique()
+        yMax = int(df.Rate.values.max()) + 1
+        xTicks = df.index.unique()
+        yTicks = np.linspace(0, yMax, 11).round(1)
+        fig, axs = plt.subplots(nrows=len(factors),
+                                ncols=len(demos), figsize=(8, 8))
 
-        fig, axs = plt.subplots(nrows=len(questions),
-                                ncols=len(groups), figsize=(8, 8))
-
-        for row in range(len(questions)):
-            rawQuestion = questions[row]
-            questionDf = groupsDf.loc[groupsDf.question == rawQuestion, :]
-            for col in range(len(groups)):
+        for row in range(len(factors)):
+            currFactor = factors[row]
+            factorDf = df.loc[df.Factor == currFactor, :]
+            for col in range(len(demos)):
                 currAx = axs[row, col]
-                currGroup = groups[col]
-                subDf = questionDf.loc[questionDf.Demography == currGroup, :]
-                currQuestion = questionDict[rawQuestion]
+                currDemo = demos[col]
+                demoDf = factorDf.loc[factorDf.Demography == currDemo, :]
                 currAx.grid(True, linestyle=self.dash, alpha=0.5)
-                print(currQuestion, currGroup)
-                print(subDf.Group.unique())
-                print('\n\n')
-                for name, group in subDf.groupby('Group'):
-                    currAx.plot(group.index, group.Value,
+                for name, subgroup in demoDf.groupby('Subgroup'):
+                    currAx.plot(subgroup.index, subgroup.Rate,
                                 label=name, marker='.')
-                currAx.legend(title=currGroup, loc='upper left', fontsize=7)
+                currAx.legend(title=currDemo, loc='lower right',
+                              title_fontsize=7.5, fontsize=6, ncol=2)
                 currAx.set_xticks(xTicks)
-                currAx.set_xticklabels(xTicks, rotation=45, fontsize=9)
+                currAx.set_xticklabels(xTicks, rotation=45, fontsize=8)
                 currAx.set_yticks(yTicks)
-                currAx.set_yticklabels(yTicks, fontsize=9)
+                currAx.set_yticklabels(yTicks, fontsize=8)
                 if (col == 0):
-                    currAx.set_ylabel(currQuestion, fontweight='bold')
+                    currAx.set_ylabel(currFactor, fontweight='bold')
                 if (row == 1):
-                    currAx.set_xlabel(subDf.index.name,
+                    currAx.set_xlabel(demoDf.index.name,
                                       fontweight='bold')
 
         fig.suptitle('Demographic Patterns of Adults with Current Asthma '
                      f'({xTicks[0]}-{xTicks[-1]})',
                      fontweight='bold')
         fig.tight_layout()
-        # plt.show()
+        plt.show()
 
     def zScoreAx(self, ax, xRange):
         ax.fill_between(xRange, -1, 1, alpha=0.1, color='gray')
@@ -168,8 +166,8 @@ class cdcAPI:
 
 
 pa = cdcAPI('PA')
-# pa.overallTrend()
 pa.demography()
+# pa.overallTrend()
 # pa.demography('Race/Ethnicity')
 # df = pa.df
 

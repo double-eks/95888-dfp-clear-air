a1adb61e8d411ce00fa13baf61e8f26dd21ce632
sean-x <xiaoxu@andrew.cmu.edu>
2023-02-28 16:42:19 -0500

    v3.6.4

--- trend.py
+++ trend.py
@@ -1,5 +1,4 @@
 import os
-from operator import index
 
 import numpy as np
 
@@ -9,23 +8,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sodapy import Socrata
 
-'''
-Asthma Indictor Question Dictionary
-    Hospitalizations for asthma
-    Asthma mortality rate
-    Emergency department visit rate for asthma
-    Asthma prevalence among women aged 18-44 years
-    Current asthma prevalence among adults aged >= 18 years
-    Influenza vaccination among noninstitutionalized adults aged >= 65 years with asthma
-    Influenza vaccination among noninstitutionalized adults aged 18-64 years with asthma
-    Pneumococcal vaccination among noninstitutionalized adults aged 18-64 years with asthma
-    Pneumococcal vaccination among noninstitutionalized adults aged >= 65 years with asthma
-'''
-
 
 class cdcAPI:
 
     def __init__(self, state: str):
+        self.start, self.end = 2011, 2020
+        self.titleSuffix = f'in {state} ({self.start}-{self.end})'
+        self.xTicks = list(range(self.start, self.end + 1))
         self.state = state
         self.dash = '--'
         self.initQuestionDict()
@@ -45,25 +34,24 @@ class cdcAPI:
     def initQuestionDict(self):
         questionDict = {
             'Asthma mortality rate':
-                ['Age-adjusted Rate', 'Asthma Mortality Rate'],
+                ['Age-adjusted Rate',
+                 'Asthma Mortality Rate'],
             'Current asthma prevalence among adults aged >= 18 years':
-                ['Age-adjusted Prevalence', 'Prevalence of Adults with Current Asthma'],
-            'Emergency department visit rate for asthma':
-                ['Age-adjusted Rate', 'Asthma ED Visit Rate'],
-            'Hospitalizations for asthma':
-                ['Age-adjusted Rate', 'Asthma Hospitalizations Rate']}
+                ['Age-adjusted Prevalence',
+                 'Prevalence of Adults with Current Asthma']}
         questionDf = pd.DataFrame(questionDict,
                                   index=['datavaluetype', 'rename'])
         self.qDict = questionDf
 
     def initClauseQuery(self):
+        time = f'((yearstart >= {self.start}) AND (yearstart <= {self.end}))'
         location = f'(locationabbr = "{self.state.upper()}") '
         data = '(datavalue IS NOT NULL)'
         quesstions = ['(question = "{}")'.format(factor)
                       for factor in list(self.qDict.columns)]
         questionClause = ' OR '.join(quesstions)
         questionClause = '({})'.format(questionClause)
-        self.clause = ' AND '.join([location, data, questionClause])
+        self.clause = ' AND '.join([time, location, data, questionClause])
 
     def convertResponse(self):
         df = pd.DataFrame.from_records(self.response)
@@ -84,7 +72,7 @@ class cdcAPI:
                                 'datavaluetype': 'Type',
                                 'datavalue': 'Rate',
                                 'datavalueunit': 'Unit',
-                                'stratificationcategory1': 'Demography',
+                                'stratificationcategory1': 'Category',
                                 'stratification1': 'Subgroup'})
         df['Factor'] = types
         df = df.set_index('Year')
@@ -119,43 +107,79 @@ class cdcAPI:
     #     plt.show()
 
     def demography(self):
-        df = self.df[self.df.Demography != 'Overall']
+        # df = self.df[self.df.Category != 'Overall']
+        df = self.df
         factors = df.Factor.unique()
-        demos = df.Demography.unique()
-        yMax = int(df.Rate.values.max()) + 1
-        xTicks = df.index.unique()
-        yTicks = np.linspace(0, yMax, 11).round(1)
+        categoriesArr = df.Category.unique()
+        subs = np.delete(categoriesArr,
+                         np.where(categoriesArr == 'Overall')[0])
         fig, axs = plt.subplots(nrows=len(factors),
-                                ncols=len(demos), figsize=(8, 8))
+                                ncols=len(subs), figsize=(8, 8))
 
         for row in range(len(factors)):
-            currFactor = factors[row]
-            factorDf = df.loc[df.Factor == currFactor, :]
-            for col in range(len(demos)):
+            rowName = 'Factor'
+            rowDf = filterDf(df, rowName, factors[row])
+            yTitle = '{} ({})'.format(rowDf.Type.unique()[0],
+                                      rowDf.Unit.unique()[0])
+            for col in range(len(subs)):
+                colName = 'Category'
+                colField = subs[col]
                 currAx = axs[row, col]
-                currDemo = demos[col]
-                demoDf = factorDf.loc[factorDf.Demography == currDemo, :]
-                currAx.grid(True, linestyle=self.dash, alpha=0.5)
-                for name, subgroup in demoDf.groupby('Subgroup'):
-                    currAx.plot(subgroup.index, subgroup.Rate,
-                                label=name, marker='.')
-                currAx.legend(title=currDemo, loc='lower right',
-                              title_fontsize=7.5, fontsize=6, ncol=2)
-                currAx.set_xticks(xTicks)
-                currAx.set_xticklabels(xTicks, rotation=45, fontsize=8)
-                currAx.set_yticks(yTicks)
-                currAx.set_yticklabels(yTicks, fontsize=8)
+                currAx.grid(True, axis='y', linestyle=self.dash, alpha=0.5)
+                fill = True if (colField == 'Gender') else False
+                self.drawGroupLines(currAx, colName, colField, rowDf, fill)
+                if (row == 0):
+                    xTitle = 'Asthma Indicators by {}'.format(colField)
+                    currAx.set_title(xTitle, fontsize=9, fontweight='bold')
                 if (col == 0):
-                    currAx.set_ylabel(currFactor, fontweight='bold')
-                if (row == 1):
-                    currAx.set_xlabel(demoDf.index.name,
-                                      fontweight='bold')
+                    currAx.set_ylabel(yTitle, fontsize=9)
 
         fig.suptitle('Demographic Patterns of Adults with Current Asthma '
-                     f'({xTicks[0]}-{xTicks[-1]})',
-                     fontweight='bold')
+                     f'{self.titleSuffix}', fontweight='bold')
         fig.tight_layout()
         plt.show()
+
+    def drawGroupLines(self, ax: plt.Axes, colName: str, colField: str,
+                       rowDf: pd.DataFrame, fill: bool = False):
+        overallDf = rowDf.loc[rowDf[colName] == 'Overall', :]
+        colDf = rowDf.loc[rowDf[colName] == colField, :]
+        subField = 'Subgroup'
+        yMax = int(rowDf.Rate.values.max()) + 3
+        yTicks = np.linspace(0, yMax, 11).round(1)
+        yValues = []
+        legendCol = 3 if (len(colDf[subField].unique()) <= 2) else 2
+
+        ax.fill_between(self.xTicks, 0, overallDf.Rate.values,
+                        color='lightgray', alpha=0.3)
+
+        ax.plot(overallDf.index, overallDf.Rate, linestyle='dashed',
+                label='Average', color='gray', linewidth=1)
+        for name, subgroup in colDf.groupby(subField):
+            line = ax.plot(subgroup.index, subgroup.Rate,
+                           label=name, marker='.')
+            yValues.append(subgroup.Rate.values)
+            lineColot = line[0].get_color()
+            self.annoateAx(ax, subgroup.Rate.values, lineColot)
+
+        if (fill):
+            ax.fill_between(self.xTicks, yValues[0], yValues[1],
+                            color='gray', alpha=0.2)
+
+        ax.set_xticks(self.xTicks)
+        ax.set_xticklabels(self.xTicks, rotation=45, fontsize=7)
+        ax.set_yticks(yTicks)
+        ax.set_yticklabels(yTicks, fontsize=7)
+        ax.legend(title=colField, loc='lower right', ncol=legendCol,
+                  title_fontsize=7, fontsize=6)
+
+    def annoateAx(self, ax: plt.Axes, yValues, lineColor):
+        xMax = self.xTicks[yValues.argmax()]
+        xMin = self.xTicks[yValues.argmin()]
+        yMax, yMin = yValues.max(), yValues.min()
+        ax.annotate(yMax, xy=(xMax, yMax), xytext=(-6, 6),
+                    textcoords='offset points', color=lineColor, fontsize=7)
+        ax.annotate(yMin, xy=(xMin, yMin), xytext=(-6, -10),
+                    textcoords='offset points', color=lineColor, fontsize=7)
 
     def zScoreAx(self, ax, xRange):
         ax.fill_between(xRange, -1, 1, alpha=0.1, color='gray')
@@ -165,7 +189,12 @@ class cdcAPI:
                    color='black', alpha=0.5)
 
 
-pa = cdcAPI('PA')
+def filterDf(df: pd.DataFrame, field: str, target: str):
+    selected = df.loc[df[field] == target, :]
+    return selected
+
+
+pa = cdcAPI('CA')
 pa.demography()
 # pa.overallTrend()
 # pa.demography('Race/Ethnicity')

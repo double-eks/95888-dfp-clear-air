6196cc263c37c07bd77088df913c0f57a0e73d87
sean-x <xiaoxu@andrew.cmu.edu>
2023-03-01 23:27:10 -0500

    v4.1

--- airnow.py
+++ airnow.py
@@ -7,7 +7,6 @@ import zipfile
 from datetime import datetime
 
 import pandas as pd
-import progressbar
 import requests
 from matplotlib.axes import Axes
 
@@ -105,55 +104,55 @@ class AirNow():
                  ]
 
 
-class EpaAqs():
-    def __init__(self, city: str, state: str) -> None:
+class EpaAQS():
+    def __init__(self) -> None:
         self.url = 'https://aqs.epa.gov/aqsweb/airdata/daily_aqi_by_cbsa_{}.zip'
-        self.initRequest(city, state)
         self.initLegendDf()
 
-    def initRequest(self, city: str, state: str):
-        dfs = []
-        for yr in progressbar.progressbar(range(2021, 2022), redirect_stdout=True):
-            url = self.url.format(yr)
-            response = requests.get(url)
-            zippedFile = zipfile.ZipFile(io.BytesIO(response.content))
-            csvFilename = zippedFile.namelist()[0]
-            csvFile = zippedFile.open(csvFilename)
-            df = pd.read_csv(csvFile)
-            csvFile.close()
-            zippedFile.close()
-            df = df.loc[df['CBSA'] == f'{city}, {state}', :]
-            df['Date'] = pd.to_datetime(df['Date'])
-            df['Day'] = df['Date'].dt.strftime('%m-%d')
-            if (len(df.index) == 366):
-                df = df.loc[df.Day != '02-29', :]
-            df = df.sort_values('Date')
-            df['Good'] = [1 if (int(aqi) <= 50) else 0
-                          for aqi in df['AQI']]
-            df['CumDays'] = df['Good'].cumsum()
-            dfs.append(df)
-        self.dfList = dfs
+    def requestSingleYr(self, yr: int, city: str, state: str):
+        # Request zip file
+        url = self.url.format(yr)
+        response = requests.get(url)
+        zippedFile = zipfile.ZipFile(io.BytesIO(response.content))
+        csvFilename = zippedFile.namelist()[0]
+        csvFile = zippedFile.open(csvFilename)
+        df = pd.read_csv(csvFile)
+        csvFile.close()
+        zippedFile.close()
+        # Clean dataframe
+        df = df.loc[df['CBSA'] == f'{city}, {state}', :]
+        df['Date'] = pd.to_datetime(df['Date'])
+        # Exculde 366
+        mmyy = df['Date'].dt.strftime('%m%d')
+        if (len(df.index) == 366):
+            df = df.loc[mmyy != '0229', :]
+        # Pre-statistic
+        df = df.sort_values('Date')
+        df = df.set_index('Date')
+
+        df['Level'] = [self.palette.index[self.palette.Category == category][0]
+                       for category in df.Category]
+        df['Month'] = df.index.month
+        df['Good Days'] = [1 if (int(aqi) <= 50) else 0 for aqi in df['AQI']]
+        df['Cum Days'] = df['Good Days'].cumsum()
+        return df
 
     def initLegendDf(self):
         result = {
-            'color': ['#00e400', '#ffff00', '#ff7e00', '#ff0000', '#8f3f97', '#7e0023'],
-            'range': [range(0, 50), range(50, 100), range(100, 150),
-                      range(150, 200), range(200, 300), range(300, 500)],
-            'category': ['Good', 'Moderate', 'Unhealthy for Sensitive Groups',
+            'Color': ['#00e400', '#ffff00', '#ff7e00', '#ff0000', '#8f3f97', '#7e0023'],
+            'Bottom': [0, 50, 100, 150, 200, 300],
+            'Cap': [50, 100, 150, 200, 300, 500],
+            'Category': ['Good', 'Moderate', 'Unhealthy for Sensitive Groups',
                          'Unhealthy', 'Very Unhealthy', 'Hazardous'],
-            'number': [i for i in range(1, 7)]
+            'Level': [i for i in range(1, 7)]
         }
         df = pd.DataFrame(result)
-        df['cap'] = [(list(levelRange)[-1] + 1)
-                     for levelRange in df.range]
-        self.aqiDict = df
+        self.palette = df.set_index('Level')
 
     def yAxisByAQI(self, ax: Axes, aqiCol: pd.Series):
         ticks = [0]
         worstAQI = aqiCol.max()
-        for row in self.aqiDict.index:
-            levelCap = self.aqiDict.cap[row]
-            levelColor = self.aqiDict.color[row]
+        for levelCap in self.palette.Cap.values:
             ticks.append(levelCap)
             # ax.axhline(y=levelCap, color=levelColor, linewidth=1, alpha=0.5)
             if (worstAQI < levelCap):
@@ -161,29 +160,15 @@ class EpaAqs():
         ax.set_yticks(ticks)
         ax.set_ylim((0, ticks[-1]))
 
-    def drawTrendPlot(self, ax: Axes, df: pd.DataFrame):
-        dates = pd.to_datetime(df['Date'])
-        worstAQI = df['AQI'].values.max()
-        worstLevelRow = self.findWorstRange(worstAQI)
-        yMax = self.aqiDict.loc[worstLevelRow, 'aqi'][1]
-        self.setFrame(ax, 'gray')
-        self.drawBackground(ax, worstLevelRow)
-        # ax.grid(True, axis='y', linestyle='dotted')
-        ax.set_xlim(dates.min(), dates.max())
-        ax.set_ylim(0, yMax)
-        ax.tick_params(axis='both', labelsize=8, color='gray')
-        ax.tick_params(axis='x', labelrotation=15)
-        ax.plot(dates, df['AQI'].values)
-
-    def drawBackground(self, ax: Axes, worstLevelRow):
-        background = 0.2
-        for row in range(worstLevelRow + 1):
-            currRange = self.aqiDict.aqi[row]
-            currColor = self.aqiDict.color[row]
-            ax.axhspan(currRange[0], currRange[1],
-                       facecolor=currColor, alpha=background)
-            if (row < worstLevelRow):
-                ax.axhline(currRange[1], color=currColor, alpha=0.5)
+    # def drawBackground(self, ax: Axes, worstLevelRow):
+    #     background = 0.2
+    #     for row in range(worstLevelRow + 1):
+    #         currRange = self.palette.aqi[row]
+    #         currColor = self.palette.color[row]
+    #         ax.axhspan(currRange[0], currRange[1],
+    #                    facecolor=currColor, alpha=background)
+    #         if (row < worstLevelRow):
+    #             ax.axhline(currRange[1], color=currColor, alpha=0.5)
 
     def setFrame(self, ax: Axes, *args):
         ax.spines['top'].set_color(*args)
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import os
+from operator import index
+from unicodedata import category
 
 os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
 
+import copy
+import logging
 import re
 from urllib.request import urlopen
 
 import matplotlib.dates as mdates
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import progressbar
 from bs4 import BeautifulSoup, Tag
 from matplotlib.axes import Axes
+from matplotlib.patches import Rectangle
 
-from airnow import AirNow, EpaAqs
+from airnow import AirNow, EpaAQS
 from console import Console
 
 
@@ -23,73 +30,173 @@ def webScraping(url: str) -> BeautifulSoup:
     return BeautifulSoup(html.read(), "lxml")
 
 
-def aqiVizPage():
-    console.header('AQI Visualization')
+def requestAQS(start: int = 2010, end: int = 2021):
+    widget = copy.copy(console.widget)
+    widget[0] = widget[0].format('EPA AQS')
+    dfList = []
+    for yr in progressbar.progressbar(range(start, end + 1),
+                                      widgets=widget, redirect_stdout=True):
+        df = apiAQS.requestSingleYr(yr, console.city, console.state)
+        dfList.append(df)
+    return pd.concat(dfList)
 
-    aqiTracker()
+
+def aqiVizPage():
+    dfs = requestAQS(start=2021)
+    singleYr = dfs.loc[(dfs.index.year == 2021), :]
+
+    # aqiTrackerByYear(singleYr, 2021, fontweight='bold', fontsize=10)
+    aqiDistribution(singleYr, 2021, fontweight='bold', fontsize=10)
     # ax.set_xlabel('Dates')
     # aqsAPI.drawTrendPlot(ax, yrDf)
     # homepage()
 
 
-def aqiTracker():
+def aqiTrackerByYear(singleYrDf: pd.DataFrame, singleYr: int, **kwargs):
     fig = plt.figure(tight_layout=True)
-    fig.set_size_inches(8, 8)
-    gs = gridspec.GridSpec(3, 4)
+    fig.set_size_inches(8, 6)
+    gs = gridspec.GridSpec(2, 2)
 
-    dailyAqiAx = fig.add_subplot(gs[0, :])
-    cumDayAx = fig.add_subplot(gs[1, :])
-    pollutantAx = fig.add_subplot(gs[2, :2])
-    tileAx = fig.add_subplot(gs[2, 2:])
+    dailyAqiAx = fig.add_subplot(gs[:1, :])
+    cumDayAx = fig.add_subplot(gs[1:, :])
 
-    dailyAqiPlot(dailyAqiAx)
-    cumGoodDayPlot(cumDayAx)
-    # levelBarAx(df, levelBarAx)
-    # monthBarAx(df, monthBarAx)
+    dailyAqiPlot(dailyAqiAx, singleYrDf['AQI'], singleYr)
+    cumGoodDayPlot(cumDayAx, singleYrDf['Cum Days'])
+
+    dailyAqiAx.legend(ncol=2, fontsize=8, loc='upper left')
+    cumDayAx.legend(fontsize=8, loc='upper left')
+    dailyAqiAx.set_ylabel('Daily AQI', **kwargs)
+    cumDayAx.set_ylabel('Cumulative Days', **kwargs)
+    dailyAqiAx.set_xlabel(f'Air Quality Annual Trend', **kwargs)
+    cumDayAx.set_xlabel(f'Cumulative Good AQI Days (AQI<50)', **kwargs)
+
+    fig.suptitle('Air Quality Time Series Analysis of Air Quality '
+                 f'in {console.city}, {console.state} ({singleYr})',
+                 fontsize=13, fontweight='bold')
+
+
+def aqiDistribution(singleYrDf: pd.DataFrame, singleYr: int, **kwargs):
+    fig = plt.figure(tight_layout=True)
+    fig.set_size_inches(8, 6)
+    gs = gridspec.GridSpec(7, 2)
+
+    monthTileAx = fig.add_subplot(gs[:3, :])
+    pollutantAx = fig.add_subplot(gs[3:6, :1])
+    scaleAx = fig.add_subplot(gs[6:, :])
+
+    categorizedBar(pollutantAx, singleYrDf,
+                   'Defining Parameter', 'Level', 20)
+    categorizedBar(monthTileAx, singleYrDf, 'Month', 'Level', 7)
+    aqiLegend(scaleAx)
+
+    monthTileAx.set_xticklabels(singleYrDf.index.unique().strftime('%b'))
+    # pollutantAx.set_ylabel('Day Count of AQI Level', **kwargs)
+
+    title = 'AQI Cumulative Days by {}'
+    monthTileAx.set_xlabel(title.format('Month'), **kwargs)
+    pollutantAx.set_xlabel(title.format(
+        'Primary Pollutant'), **kwargs)
+    scaleAx.set_xlabel('Air Quality Index (AQI) Categorization', **kwargs)
+
+    fig.suptitle('Air Quality Time Series Analysis of Air Quality '
+                 f'in {console.city}, {console.state} ({singleYr})',
+                 fontsize=13, fontweight='bold')
+
+
+def dailyAqiPlot(ax: Axes, sAQI: pd.Series, yr: int):
+    moving = 7
+    lagging = 14
+    movingAvg = sAQI.rolling(window=moving).mean()
+    laggingAvg = sAQI.rolling(window=lagging).mean()
+    avgMax = (movingAvg.max() // 50 + 1) * 50
+
+    ax.grid(True, linestyle='dotted')
+    ax.plot(sAQI.index, laggingAvg, linewidth=10, alpha=0.3,
+            label=f'{lagging}-lagging-day moving average', color='gray')
+    ax.plot(sAQI.index, movingAvg, linewidth=3, alpha=0.8,
+            label=f'{moving}-day moving average', color='deepskyblue')
+    ax.axhline(avgMax, 0, 1, linestyle='dashed',
+               label='Worst cap of moving average', color='#ff7e00')
+    ax.plot(sAQI, linewidth=1.2, label=str(yr), color='steelblue')
+
+    apiAQS.yAxisByAQI(ax, sAQI)
+    fmtDateAxis(ax, sAQI)
     return
 
 
-def dailyAqiPlot(ax: Axes):
-    df = aqsAPI.dfList[-1]
-    movingAvg = df['AQI'].rolling(window=7).mean()
-    biWeekAvg = df['AQI'].rolling(window=14).mean()
-    dateLim = (df['Date'].min()), (df['Date'].max())
-    ax.grid(True, linestyle='dotted')
-    ax.plot(df['Date'], movingAvg)
-    ax.plot(df['Date'], biWeekAvg)
-    ax.plot(df['Date'], df['AQI'])
-    aqsAPI.yAxisByAQI(ax, df['AQI'])
-    fmtSharingAx(ax, dateLim)
-    return
-    # ax.set_ylabel('Daily AQI Value', fontweight='bold')
+def cumGoodDayPlot(ax: Axes, sCumDay: pd.Series):
+    interval = 50
+    sumDay = sCumDay.max()
+    breakPoint = sumDay // interval * interval
+    goodDays = [day for day in range(0, breakPoint, interval)]
+    badDays = [day for day in range(breakPoint, 365, interval)]
+    dayTicks = copy.copy(goodDays)
+    dayTicks.extend(badDays[1:])
+    dayTicks.extend([sumDay, 365])
 
-
-def cumGoodDayPlot(ax: Axes):
-    df = aqsAPI.dfList[-1]
-    dateLim = (df['Date'].min()), (df['Date'].max())
     ax.grid(True, linestyle='dotted')
-    ax.plot(df['Date'], df['CumDays'])
-    yrDays = [i for i in range(0, 360, 30)]
-    yrDays.append(365)
+    ax.fill_between(sCumDay.index, sumDay, 365, alpha=0.1,
+                    color='#ff7e00')
+    ax.axhline(sumDay, 0, 1, color='#ff7e00')
+    ax.plot(sCumDay, linewidth=3,
+            label='Cumulative Good Days (AQI<50)', color='steelblue')
+
     ax.set_ylim((0, 365))
-    ax.set_yticks(yrDays)
-    fmtSharingAx(ax, dateLim)
+    ax.set_yticks(dayTicks)
+    fmtDateAxis(ax, sCumDay)
     return
 
 
-def fmtSharingAx(ax: Axes, dateLim: tuple):
-    ax.tick_params(axis='both', labelsize=8, color='gray')
-    ax.tick_params(axis='x', rotation=30)
+def fmtDateAxis(ax: Axes, s: pd.Series):
+    dateLim = (s.index[0], s.index[-1])
     ax.set_xlim(dateLim)
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    ax.tick_params(axis='both', labelsize=8)
+    ax.tick_params(axis='x', rotation=15)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
 
 
-def levelCountBar(df: pd.DataFrame, ax: Axes):
+def categorizedBar(ax: Axes, df: pd.DataFrame, xField: str, yField: str,
+                   labelLimit: int):
+    width = 0.5
+    for xName, group in df.groupby(xField):
+        bottom = 0
+        for yName, subgroup in group.groupby(yField):
+            color = aqiPalette.Color[yName]
+            v = len(subgroup.index)
+            b = ax.bar(xName, v, width,
+                       label=yName, color=color, bottom=bottom)
+
+            bottom += v
+            if (v > labelLimit):
+                ax.bar_label(b, label_type='center', fontsize=8)
+    ax.set_xticks(df[xField].values)
+    ax.tick_params(labelsize=8)
     return
 
 
-def monthCumBar(df: pd.DataFrame, ax: Axes):
-    return
+def aqiLegend(ax: Axes):
+    xTicks = [0]
+    ax.set_yticks([])
+    ax.set_ylim((0, 3))
+    for i in aqiPalette.index:
+        color = aqiPalette.Color[i]
+        x1 = aqiPalette.Bottom[i]
+        x2 = aqiPalette.Cap[i]
+        scale = aqiPalette.Category[i]
+        xTicks.append(x2)
+        ax.fill_betweenx(np.linspace(0, 1), x1, x2, color=color)
+        if (scale == 'Unhealthy for Sensitive Groups'):
+            scale = 'Sensitive'
+        ax.text(x1, 1.5, scale, fontsize=8)
+    # levelCap.append(aqiPalette.Cap.iloc[-1])
+    ax.set_xticks(xTicks)
+    ax.set_xlim((xTicks[0], xTicks[-1]))
+    ax.tick_params(labelsize=8)
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['right'].set_color('none')
+    # Rectangle((bottom, 0), cap, 10, color=color, fill=True)
 
 
 def homepage(new: bool = False):
@@ -100,9 +207,8 @@ if __name__ == "__main__":
 
     console = Console()
 
-    console.loading('EPA Air Quality System')
-    aqsAPI = EpaAqs(console.city, console.state)
-
+    apiAQS = EpaAQS()
+    aqiPalette = apiAQS.palette
     aqiVizPage()
 
     plt.show()
